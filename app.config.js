912d62f2597d4d22342fead0cf7b05b53944f33f
Claims IQ <john@claimsiq.ai>
2026-02-15 22:12:39 +0000

    sdfsdf

--- app.config.js
+++ app.config.js
@@ -1,8 +1,12 @@
 export default {
   expo: {
     ...require("./app.json").expo,
+    owner: "claimsiq",
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:5000",
+      eas: {
+        projectId: "e70b8d1c-02fc-429d-b1c1-476bba398da3",
+      },
     },
   },
 };
