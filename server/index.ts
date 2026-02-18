c4189b0b315239ae6a16618b3f95e78296387031
Claims IQ <john@claimsiq.ai>
2026-02-18 16:56:40 +0000

    sdf

--- server/index.ts
+++ server/index.ts
@@ -28,7 +28,9 @@ app.use(
             directives: {
               defaultSrc: ["'self'"],
               scriptSrc: ["'self'"],
-              styleSrc: ["'self'", "'unsafe-inline'"],
+              styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+              styleSrcElem: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+              fontSrc: ["'self'", "https://fonts.gstatic.com"],
               imgSrc: ["'self'", "data:", "blob:", "*.supabase.co"],
               connectSrc: [
                 "'self'",
